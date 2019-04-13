de3af7f15282df4c068939eb88e5f1aa35d6184d
jwzheng <511894306@qq.com>
2019-04-13 14:00:22 +0800

    27 二叉树的镜像

---
+++ 27_二叉树的镜像/main.cpp
@@ -0,0 +1,23 @@
+/*
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};*/
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+        if (pRoot == nullptr)
+            return;
+        // 递归交换左右子树
+        Mirror(pRoot->left);
+        Mirror(pRoot->right);
+        TreeNode *tmp = pRoot->left;
+        pRoot->left = pRoot->right;
+        pRoot->right = tmp;
+        return;
+    }
+};
