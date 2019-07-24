02c7315e6663a41566b3bd4cb165ff49c152aa2b
jwzheng <511894306@qq.com>
2019-07-24 11:58:56 +0800

    增加递归公式求解约瑟夫环方法

--- 62_圆圈中最后剩下的数字/main.cpp
+++ 62_圆圈中最后剩下的数字/main.cpp
@@ -33,6 +33,21 @@ public:
     }
 };
 
+// 递归公式求解
+class Solution {
+public:
+    int LastRemaining_Solution(int n, int m){
+        if(n<1||m<1)
+            return -1;
+
+        int last = 0;
+        for (int i = 2; i<=n; ++i){
+            last = (last+ m) % i;
+        }
+        return last;
+    }
+};
+
 int main(int argc, char** argv) {
     Solution sol;
     cout<<sol.LastRemaining_Solution(5,3)<<endl;
